75ae75736db7c63a73d61a4bff02cb728df23eef
AneeqaAdil <official.aneeqa11@gmail.com>
2026-09-02 15:44:24 +0530

    Practicing Pattern-printing questions

---
+++ Q56.java
@@ -0,0 +1,22 @@
+// Print the following pattern for N = 5
+
+import java.util.Scanner;
+
+class Main {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        for (int i = 1; i <= n; i++) {
+
+            for (int j = 1; j <= n; j++) {
+                System.out.print("* ");
+            }
+
+            System.out.println();
+        }
+    }
+}
